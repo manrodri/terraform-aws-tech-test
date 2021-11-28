fcfee5bdb4f79c38bd8afab003ba0517fb4177f7
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-11-28 14:45:59 +0000

    only dynamodb.tf left

--- bastion.tf
+++ bastion.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "bastion" {
   key_name                    = aws_key_pair.web.key_name
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-  subnet_id = module.vpc.public_subnets[0]
-  security_groups = [aws_security_group.bastion-sg.id]
-  tags = merge(local.common_tags, {"Name": "${terraform.workspace}-bastion"})
+  subnet_id                   = module.vpc.public_subnets[0]
+  security_groups             = [aws_security_group.bastion-sg.id]
+  tags                        = merge(local.common_tags, { "Name" : "${terraform.workspace}-bastion" })
 
 }
